53793f5f5b0ab6d08d74098d88cb32cfb4dcc594
tyler-gotz <tyler.gotz@teschglobal.com>
2022-07-01 12:04:42 -0500

    connected redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,22 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MantineProvider } from '@mantine/core'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MantineProvider
-      withNormalizeCSS
-      withGlobalStyles
-      theme={{
-        fontFamily: 'Arima',
-        colorScheme: 'light',
-        colors: {
-          brand: ['#de6b48', '#594F3B', '#119DA4', '#FFFD82', '#F6F8FF'],
-        },
-        primaryColor: 'brand',
-      }}
-    >
-      <App />
-    </MantineProvider>
+    <Provider store={store}>
+      <MantineProvider
+        withNormalizeCSS
+        withGlobalStyles
+        theme={{
+          fontFamily: 'Arima',
+          colorScheme: 'light',
+          colors: {
+            brand: ['#de6b48', '#594F3B', '#119DA4', '#FFFD82', '#F6F8FF'],
+          },
+          primaryColor: 'brand',
+        }}
+      >
+        <App />
+      </MantineProvider>
+    </Provider>
   </React.StrictMode>
 )
